4f01e7deee32d5df355d7daefe3b8c59c5aea748
OzPg <bruno_apx@outlook.com>
2024-01-03 21:18:33 -0300

    Aula
    Aula

---
+++ Python/Aula34.py
@@ -0,0 +1,14 @@
+"""
+https://docs.python.org/3/library/stdtypes.html Toda a documentação do Python
+Imutáveis que vimos: str, int, float e bool
+"""
+
+string = 'Bruno Alves'
+# e possivel fazer essa troca, porém como e um valor imutavel não e possivel alterar ele.
+
+#string[3] = 'ABC' 
+#outra_varavel = f'{string[:3]}ABC{string[5:]}'
+#print(string)
+#print(outra_varavel)
+
+print(string.zfill(10))
