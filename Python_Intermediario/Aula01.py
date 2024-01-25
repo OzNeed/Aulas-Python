2d3902aba023cc8c0c423e39d665892f2c6ddad9
OzPg <bruno_apx@outlook.com>
2024-01-25 14:17:38 -0300

    Atualização e nova aula
    Novas aulas

--- Python_Intermediario/Aula01.py
+++ Python_Intermediario/Aula01.py
@@ -6,4 +6,32 @@ Elas podem receber valores para parâmetros (argumentos)
 e retornar um valor específico.
 Por padrão, funções Python retornam None (nada)
 """
-print ('Várias')
+# o Print tem parametros que em determinado momento vai receber algum argumento
+
+def Print(a, b, c):
+    print ('Várias1')
+    print ('Várias2')
+    print ('Várias3')
+    print ('Várias4')
+print('')
+print('')
+#quando se referir ao parâmetro, está falando o que vai dentro do parentese no def.
+def imprimir(a, b, c):
+    print(a, b, c)
+
+# #quando está se referindo ao valor, e o valor que e passado dentro do parentese pedindo para executar.
+imprimir(1, 2, 3)
+imprimir(4, 5, 6)
+#caso o valor seja definido dentro do def, ai vai mostrar o que foi alimento dentro do executor.
+#pode ser alterado quando quiser os parâmetros, quando for chamado ele
+print('')
+print('')
+#pode também definir um valor dentro do def, para não ter erro ao chamar ele
+#pois ele e como se fosse uma váriavel
+def saudacao(nome='Sem nome'):
+    print(f'Olá, {nome}')
+
+saudacao('Bruno Alves')
+saudacao('Bruno')
+saudacao('Bruna')
+saudacao()
---
+++ Python_Intermediario/Aula02.py
@@ -0,0 +1,5 @@
+"""
+Argumentos nomeados e não nomeados em funções Python
+Argumentos nomeado tem nome com sinal de igual
+Argumento não nomeado recebe apenas o argumento (valor)
+"""
