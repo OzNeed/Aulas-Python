d3128d7d0e456327f7ee98265fcffec57dbeb640
OzPg <bruno_apx@outlook.com>
2024-02-01 22:42:43 -0300

    Aula nova
    Aula nova

---
+++ Python_Intermediario/Aula06.py
@@ -0,0 +1,17 @@
+"""
+Retorno de valores das funções (return)
+"""
+def soma(x, y):
+    print(x + y)
+
+# variavel = soma(1, 2)
+# variavel = int('1')
+
+#esse tipo de fuynção não armazena os dados para mostra ou realizar outra ação posterior com o resultado.
+soma1 = soma(2, 2)
+soma2 = soma(3, 3)
+
+#tem funções que tem a função para mostrar valores e não tem nenhum retorno de valor
+#tem funções esapecificas para retornar valores.
+
+print(soma1 + soma2)
